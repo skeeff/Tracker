7b36173ebbe080562c655eadf6cd3bb90c033b12
Fedor Chistovskii <fedorch2012@gmail.com>
2025-08-11 19:50:58 +0300

    smthWROng w coreData

---
+++ Tracker/CoreData/Storage/TrackerCategoryStore.swift
@@ -0,0 +1,132 @@
+import UIKit
+import CoreData
+
+protocol TrackerCategoryStoreDelegate: AnyObject {
+    func didUpdateCategory()
+}
+
+protocol TrackerCategoryStoreProtocol: AnyObject {
+    var categories: [TrackerCategory] { get }
+    var delegate: TrackerCategoryStoreDelegate? { get set }
+    func createCategory(_ trackerCategory: TrackerCategory)
+    func addTrackerToCategory(tracker: Tracker, category: String)
+    func deleteCategory(_ category: String)
+}
+
+final class TrackerCategoryStore: NSObject, TrackerCategoryStoreProtocol, NSFetchedResultsControllerDelegate {
+    private let context: NSManagedObjectContext
+    private let appDelegate: AppDelegate
+    private let trackerStore: TrackerStoreProtocol
+    
+    weak var delegate: TrackerCategoryStoreDelegate?
+    
+    private lazy var fetchedResultsController: NSFetchedResultsController<TrackerCategoryCoreData> = {
+        let fetchRequest = TrackerCategoryCoreData.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \TrackerCategoryCoreData.name,
+                                                         ascending: true)]
+        
+        let fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+        fetchedResultsController.delegate = self
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            print("Error fetching TrackerCategoryCoreData: \(error)")
+        }
+        
+        return fetchedResultsController
+    }()
+    
+    var categories: [TrackerCategory] {
+        guard let categoriesCoreData = fetchedResultsController.fetchedObjects else { return [] }
+        return categoriesCoreData.compactMap { getCategory(from: $0)}
+    }
+    
+    init(context: NSManagedObjectContext, appDelegate: AppDelegate, trackerStore: TrackerStoreProtocol) {
+        self.context = context
+        self.appDelegate = appDelegate
+        self.trackerStore = trackerStore
+        super.init()
+    }
+    
+    convenience init(trackerStore: TrackerStoreProtocol) {
+        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate)
+        else {
+            assertionFailure("AppDelegate not found")
+            self.init(trackerStore: trackerStore)
+            return
+        }
+        
+        self.init(
+            context: appDelegate.persistentContainer.viewContext,
+            appDelegate: appDelegate,
+            trackerStore: trackerStore
+        )
+    }
+    
+    //MARK: Protocol methods
+    func createCategory(_ trackerCategory: TrackerCategory) {
+        let categoryCoreData = TrackerCategoryCoreData(context: context)
+        categoryCoreData.name = trackerCategory.category
+        appDelegate.saveContext()
+        delegate?.didUpdateCategory()
+    }
+    
+    func addTrackerToCategory(tracker: Tracker, category: String) {
+        print(#function)
+        
+        let trackerCoreData = trackerStore.createTracker(with: tracker)
+        
+        let categoryCoreData = getCategoryCoreData(from: category) ?? {
+            let newCategory = TrackerCategoryCoreData(context: context)
+            newCategory.name = category
+            return newCategory
+        }()
+        
+        trackerCoreData.category = categoryCoreData
+        
+        appDelegate.saveContext()
+        delegate?.didUpdateCategory()
+        delegate?.didUpdateCategory()
+    }
+    
+    func deleteCategory(_ category: String) {
+        guard let trackerCategoryCoreData = getCategoryCoreData(from: category) else { return }
+        context.delete(trackerCategoryCoreData)
+        appDelegate.saveContext()
+        delegate?.didUpdateCategory()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        delegate?.didUpdateCategory()
+    }
+    
+    //MARK: - private methods
+    
+    private func getCategory(from categoryCoreData: TrackerCategoryCoreData) -> TrackerCategory? {
+        guard
+            let name = categoryCoreData.name,
+            let trackers = categoryCoreData.trackers as? Set<TrackerCoreData>
+        else { return nil }
+        
+        return TrackerCategory(
+            category: name,
+            trackers: trackers.compactMap { trackerStore.getTracker(from: $0) }
+        )
+    }
+    
+    private func getCategoryCoreData(from category: String) -> TrackerCategoryCoreData? {
+        let request = fetchedResultsController.fetchRequest
+        request.predicate = NSPredicate(format: "name == %@", category as CVarArg)
+        do {
+            return try context.fetch(request).first
+        } catch {
+            print("Error fetching TrackerCategoryCoreData: \(error)")
+            return nil
+        }
+    }
+}
---
+++ Tracker/CoreData/Storage/TrackerRecordStore.swift
@@ -0,0 +1,90 @@
+import Foundation
+import CoreData
+
+protocol TrackerRecordDelegate: AnyObject {
+    func didUpdateRecord()
+}
+
+protocol TrackerRecordStoreProtocol {
+    func addRecord(forTrackerID trackerID: UUID, date: Date) throws
+    func removeRecord(forTrackerID trackerID: UUID, date: Date) throws
+    func isTrackerCompletedToday(trackerID: UUID, date: Date) throws -> Bool
+    func getCompletedCount(forTrackerID trackerID: UUID) throws -> Int
+    func getCompletedRecords() throws -> Set<TrackerRecord>
+    var delegate: TrackerRecordDelegate? { get set }
+}
+
+final class TrackerRecordStore: NSObject, TrackerRecordStoreProtocol {
+    
+    private let context : NSManagedObjectContext
+    
+    weak var delegate: TrackerRecordDelegate?
+    
+    init(context: NSManagedObjectContext) {
+        self.context = context
+        super.init()
+    }
+    //MARK: Protocol methods
+    func addRecord(forTrackerID trackerID: UUID, date: Date) throws {
+        let fetchRequest: NSFetchRequest<TrackerCoreData> = TrackerCoreData.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", trackerID as CVarArg)
+        
+        let trackers = try context.fetch(fetchRequest)
+        if let tracker = trackers.first {
+            let recordCoreData = TrackerRecordCoreData(context: context)
+            recordCoreData.id = UUID()
+            recordCoreData.date = date
+            recordCoreData.trackerOwner = tracker
+            try context.save()
+            delegate?.didUpdateRecord()
+        }
+        
+    }
+    
+    func removeRecord(forTrackerID trackerID: UUID, date: Date) throws {
+        let request = TrackerRecordCoreData.fetchRequest()
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: date)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+        // ✅ ИСПРАВЛЕНО: Используем `trackerOwner.id` для связи с трекером.
+        request.predicate = NSPredicate(format: "trackerOwner.id == %@ AND date >= %@ AND date < %@", trackerID as CVarArg, startOfDay as CVarArg, endOfDay as CVarArg)
+        let records = try context.fetch(request)
+        if let recordToDelete = records.first {
+            context.delete(recordToDelete)
+            try context.save()
+            delegate?.didUpdateRecord()
+        }
+        
+    }
+    
+    func isTrackerCompletedToday(trackerID: UUID, date: Date) throws -> Bool {
+        let request = TrackerRecordCoreData.fetchRequest()
+        let calendar = Calendar.current
+        let startOfDay = calendar.startOfDay(for: date)
+        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+        request.predicate = NSPredicate(format: "trackerOwner.id == %@ AND date >= %@ AND date < %@", trackerID as CVarArg, startOfDay as CVarArg, endOfDay as CVarArg)
+        
+        let count = try context.count(for: request)
+        return count > 0
+    }
+    
+    func getCompletedCount(forTrackerID trackerID: UUID) throws -> Int {
+        let request = TrackerRecordCoreData.fetchRequest()
+        request.predicate = NSPredicate(format: "trackerOwner.id == %@", trackerID as CVarArg)
+        
+        return try context.count(for: request)
+        
+    }
+    func getCompletedRecords() throws -> Set<TrackerRecord> {
+        let request: NSFetchRequest<TrackerRecordCoreData> = TrackerRecordCoreData.fetchRequest()
+        let coreDataRecords = try context.fetch(request)
+        
+        let records = coreDataRecords.compactMap { coreDataRecord -> TrackerRecord? in
+            guard let trackerID = coreDataRecord.trackerOwner?.id,
+                  let recordDate = coreDataRecord.date else { return nil }
+            return TrackerRecord(id: trackerID, date: recordDate)
+        }
+        return Set(records)
+    }
+    
+}
